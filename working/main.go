18aee40eacf8ae44a3fbd669642d2354e2e1859f
Mohammad Bilal <mdbilal7620@gmail.com>
2024-12-06 00:23:45 +0530

    handling error status

--- working/main.go
+++ working/main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
-		log.Println("Hello Go!")
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+
+		data, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(rw, "Oops", http.StatusBadRequest)
+			return
+		}
+		fmt.Fprintf(rw, "Hello from %s", data)
 	})
 
 	http.HandleFunc("/bye", func(http.ResponseWriter, *http.Request) {
